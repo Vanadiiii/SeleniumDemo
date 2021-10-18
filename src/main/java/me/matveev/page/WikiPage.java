556a77a431ab8393b066142ffbe813e09c87b702
Matveev Ivan <vanadiiii42@gmail.com>
2021-10-18 22:20:05 +0400

    Selectors

--- src/main/java/me/matveev/page/WikiPage.java
+++ src/main/java/me/matveev/page/WikiPage.java
@@ -1,6 +1,9 @@
 package me.matveev.page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -12,9 +15,23 @@ public class WikiPage {
         capabilities.setCapability("marionette", true);
 
         WebDriver driver = new FirefoxDriver(capabilities);
+        driver.manage()
+                .window()
+                .maximize();
 
-        driver.navigate().to("https://www.google.com");
+        try {
+            driver.navigate().to("https://www.google.com");
 
-        driver.quit();
+            WebElement searchInputElement = driver.findElement(By.cssSelector("input[name='q']"));
+            searchInputElement.sendKeys("hello selenium");
+
+            WebElement searchButton = driver.findElements(By.name("btnK")).get(1);
+            searchButton.click();
+
+        } catch (WebDriverException e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
     }
 }
